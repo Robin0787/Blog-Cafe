400031eadf7cb24a62b13026671ceb9197ff6f88
Robin <mohammadrobin636@gmail.com>
2023-06-15 13:32:25 +0600

    Fixed some bugs

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
     return (
         <nav className='flex justify-between items-center my-3'>
             <h1 className="text-3xl font-semibold">Blog Cafe</h1>
-            <div>
-                <a href="/home" className='text-sm text-black p-2 hover:text-blue-600 duration-300'>HOME</a>
-                <a href="/SignUP" className='text-sm text-black p-2 hover:text-blue-600 duration-300'>SIGN UP</a>
-                <a href="/login" className='text-sm text-black p-2 hover:text-blue-600 duration-300'>LOGIN</a>
+            <div className='flex justify-center items-center gap-3'>
+                <p className='text-sm text-black cursor-pointer p-2 hover:text-blue-600 duration-300'>HOME</p>
+                <p className='text-sm text-black cursor-pointer p-2 hover:text-blue-600 duration-300'>SIGN UP</p>
+                <p className='text-sm text-black cursor-pointer p-2 hover:text-blue-600 duration-300'>LOGIN</p>
             </div>
         </nav>
     );
